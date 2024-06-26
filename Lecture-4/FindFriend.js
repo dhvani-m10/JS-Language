681bcda0b91dcd1d069884bb35d0bc57b9e94af6
dhvani-m10 <dhvanimoradiya10@gmail.com>
2024-06-26 15:15:23 +0530

    Conditional

---
+++ Lecture-4/FindFriend.js
@@ -0,0 +1,11 @@
+let n = 12
+
+if (n < 13) {
+    console.log("1 KM");
+} else if (n >= 13 && n < 18) {
+    console.log("5 KM");
+} else if (n >= 18 && n < 30) {
+    console.log("10 KM");
+} else {
+    console.log("YOU CAN HAVE FRIENDS FROM ANYWHERE!");
+}
