395b755f879cee9bb06b80847787f2e261d4a4ea
dhvani-m10 <dhvanimoradiya10@gmail.com>
2024-06-28 16:32:35 +0530

    Function

--- Lecture-6/Variable.js
+++ Lecture-6/Variable.js
@@ -1,9 +1,9 @@
 const variable = () => {
-    
-    let num1 = document.getElementById('num1').value;
-    let num2 = document.getElementById('num2').value;
 
+    let num1 = document.getElementById('num1').value;
     document.getElementById('res').innerHTML = `${num1}`;
-    document.getElementById('res').innerHTML = `${num2}`
+
+    num1 += "<br>" + document.getElementById('num2').value;
+    document.getElementById('res').innerHTML = `${num1}`;
 
 }
