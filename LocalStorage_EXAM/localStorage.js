f098be37e8dc49bb1c5170281161afa4f2c2f732
dhvani-m10 <dhvanimoradiya10@gmail.com>
2024-08-12 21:46:11 +0530

    Local Storage

--- LocalStorage_EXAM/localStorage.js
+++ LocalStorage_EXAM/localStorage.js
@@ -1,43 +1,53 @@
-let users = [];
-let localStorage=()=>{
-    document.getElementById("tbody").innerHTML = "";
-    for (let i = 0; i < users.length; i++) {
-      let tr = document.createElement("tr");
-      let td1 = document.createElement("td");
-      td1.innerHTML = users[i].sname;
-      let td2 = document.createElement("td");
-      td2.innerHTML = users[i].age;
-      let td3 = document.createElement("td");
-      td3.innerHTML = users[i].grid;
-      let td4 = document.createElement("td");
-      td4.innerHTML = users[i].number;
-      let td5 = document.createElement("td");
-      td5.innerHTML=users[i].price;
-    //   let td6 = document.createElement("td");
-    //   td6.innerHTML = "Delete";
-    //   td6.style.color = "red";
-    //   td6.addEventListener("click", () => handleDelete(i));
-    
-      tr.append(td1, td2, td3, td4, td5);
-      document.getElementById("tbody").append(tr);
-    }
+let users = JSON.parse(localStorage.getItem('users')) || [];
+
+let showTable = () => {
+  document.getElementById("tbody").innerHTML = "";
+  for (let i = 0; i < users.length; i++) {
+    let tr = document.createElement("tr");
+    let td1 = document.createElement("td");
+    td1.innerHTML = users[i].sname;
+    let td2 = document.createElement("td");
+    td2.innerHTML = users[i].age;
+    let td3 = document.createElement("td");
+    td3.innerHTML = users[i].grid;
+    let td4 = document.createElement("td");
+    td4.innerHTML = users[i].number;
+
+    tr.append(td1, td2, td3, td4);
+    document.getElementById("tbody").append(tr);
+  }
 }
 
-let Data = (e) => {
-    e.preventDefault();
+let addUser = (e) => {
+  e.preventDefault();
 
-    let user={
-         sname:document.getElementById('sname').value,
-        age:document.getElementById('age').value,
-        grid:document.getElementById('grid').value,
-        number:document.getElementById('number').value,
-    }
+  let user = {
+    sname: document.getElementById('sname').value,
+    age: document.getElementById('age').value,
+    grid: document.getElementById('grid').value,
+    number: document.getElementById('number').value,
+  }
+  if (sname.length < 2) {
+    alert("Please enter a valid name");
+    return;
+  }
+  if (!age) {
+    alert("Please enter Age");
+    return;
+  }
+  if (!number) {
+    alert("Please enter a valid Number");
+    return;
+  }
 
-    users.push(user);
-    console.log(users);
-    localStorage();
-    
 
+  users.push(user);
+  localStorage.setItem('users', JSON.stringify(users));
+  showTable();
+  document.getElementById("localstorage").reset();
 }
 
-document.getElementById("locatstorage").addEventListener("submit", Data);
+document.getElementById("localstorage").addEventListener("submit", addUser);
+
+window.onload = showTable;  
+
