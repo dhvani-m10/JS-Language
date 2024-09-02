6970fa12cc2f651d1734e9fcdd38366fe52a3c14
dhvani-m10 <dhvanimoradiya10@gmail.com>
2024-09-02 16:43:39 +0530

    Project

--- Project/JS/signup.js
+++ Project/JS/signup.js
@@ -1,6 +1,21 @@
+import getValue from "../components/helper";
+import Navbar from "../components/navbar";
 
+
+document.getElementById("navbar").innerHTML = Navbar()
+let users = JSON.parse(localStorage.getItem("users")) || []
 const handleData=(e)=>{
     e.preventDefault();
+
+    let user={
+        username:getValue('.username'),
+        email:getValue('.email'),
+        password:getValue('.password'),
+    };
+    users.push(user);
+    localStorage.setItem("users", JSON.stringify(users));
+
+    window.location.href="./home.html"
 }
 
 document.querySelector('userData').addEventListener("submit",handleData);
